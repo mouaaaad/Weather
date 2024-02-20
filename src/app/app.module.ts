5ed3bf0efc1d3ddb4565337be39d6d19b10044f7
mbenfedane <mbenfedane@NEOS-NBK915>
2024-02-20 15:28:24 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { NavbarsComponent } from './navbars/navbars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    NavbarsComponent
   ],
   imports: [
     BrowserModule
